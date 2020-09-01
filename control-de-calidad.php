a0a590b4ef7af4ace62ffe77cd9bed44cafbb3b5
Your NameBrayam <bvalerop@gmail.com>
2020-09-01 16:15:41 -0500

    Minor changes

--- control-de-calidad.php
+++ control-de-calidad.php
@@ -109,7 +109,7 @@ if (is_null($capacidad_total[0]['CAPACIDAD_TOTAL'])) {
 
     <!-- Mostramos las tablas con la información correspondiente -->
     <div class="table-responsive text-center">
-        <table class="table table-bordered text-center" id="tabla">
+        <table class="table table-bordered text-center">
 
             <!-- Primer <TH> -->
             <thead class="thead-dark">
@@ -606,27 +606,6 @@ if (is_null($capacidad_total[0]['CAPACIDAD_TOTAL'])) {
 
 <!-- Inline JavaScript -->
 <script>
-
-// DataTables Plugin: https://datatables.net/
-const tabla = $('#tabla').DataTable({
-    info: false,
-    dom: "lrtip",
-    // searching: false,
-    lengthChange: false,
-    pageLength: 5,
-    order: [[0, 'desc']],
-    columnDefs: [{
-        targets: 4,
-        searchable: true,
-        orderable: true,
-        className: "align-middle", "targets": "_all"
-    }],
-    language: {
-        "url": "<?= BASE_URL . "datatables/Spanish.json"; ?>"
-    }
-});
-
-
 
 // Declaramos las Variables y Constantes
 var numero;
