e31417c9938dab3dd99db3acec595723a9ec1757
Your NameBrayam <bvalerop@gmail.com>
2020-09-05 16:57:05 -0500

    minor changes

--- ver-etiquetas.php
+++ ver-etiquetas.php
@@ -23,9 +23,9 @@ if(!in_array($_SESSION['USUARIO']['CARGO'], $roles_permitidos)){
     $id = $_GET['id'];
 ?>
 
-<div class="container-fluid text-center">
+<div class="text-center">
     <button class="btn btn-main hide-on-print my-4" type="button" onclick="printDiv('printableArea')">Imprimir Etiquetas</button>
-    <div class="row contenedorEtiquetas" id="printableArea"></div>
+    <div class="contenedorEtiquetas" id="printableArea"></div>
 </div>
 
 <script>
@@ -41,63 +41,88 @@ $.ajax({
 
         const result = JSON.parse(data);
 
-        for (let index = 0; index < result.length; index++) {
+        let count = 0;
+        let pageBreak = 0;
 
-            console.log(index);
+        for (let index = 0; index < result.length; index++) {
 
             result.forEach(row => {
             
-            let cantidad =  parseInt(row.CANTIDAD);
-            let capacidad = parseInt(row.CAP_EMPAQUETADO);
+                let cantidad =  parseInt(row.CANTIDAD);
+                let capacidad = parseInt(row.CAP_EMPAQUETADO);
 
-            while (cantidad > capacidad) {
-                
+                while (cantidad > capacidad) {
+
+                    count++;
+                    
+                    $('.contenedorEtiquetas').append(`
+                    <div class="col-6 mb-4">
+                        <div class="card">
+                            <div class="text-center pt-4 pb-2">
+                                <p>Paquete ${count}</p>
+                                <div class="btn btn-lg btn-dark mb-2">PEDIDO ${row.PEDIDO_ID}</div>
+                                <h1 class="font-weight-bold">${row.NOMBRE.toUpperCase()}</h1>
+                                <h3>${row.MARCA.toProperCase()} ${row.COLOR.toProperCase()}</h3>
+                                <div class="mt-3">
+                                    <h1 class="text-dark font-weight-bold py-3 mb-0 border-bottom" style="font-size: 100px;">${row.TALLA}</h1>
+                                    <h1 class="text-dark font-weight-bold pt-3 mb-0" style="font-size: 100px;">${capacidad}</h1>
+                                </div>
+                            </div>
+                        </div>  
+                    </div>
+                    `);
+
+                    cantidad = cantidad - capacidad;
+
+                    if(pageBreak == 1){
+                        $('.contenedorEtiquetas').append(`
+                            <div class="page-break"></div> 
+                        `);
+                        pageBreak = 0;
+                    } else {
+                        pageBreak ++;
+                    }
+
+                    console.log(pageBreak);
+
+                }
+
+                count++;
+
                 $('.contenedorEtiquetas').append(`
                 <div class="col-6 mb-4">
                     <div class="card">
                         <div class="text-center pt-4 pb-2">
-                            <p>${index} de a ${result.length}</p>
-                            
+                            <p>Paquete ${count}</p>
                             <div class="btn btn-lg btn-dark mb-2">PEDIDO ${row.PEDIDO_ID}</div>
                             <h1 class="font-weight-bold">${row.NOMBRE.toUpperCase()}</h1>
                             <h3>${row.MARCA.toProperCase()} ${row.COLOR.toProperCase()}</h3>
                             <div class="mt-3">
-                                <h1 class="text-dark font-weight-bold py-3 mb-0 border-bottom" style="font-size: 85px;">${row.TALLA}</h1>
-                                <h1 class="text-dark font-weight-bold pt-3 mb-0" style="font-size: 85px;">${capacidad}</h1>
+                                <h1 class="text-dark font-weight-bold py-3 mb-0 border-bottom" style="font-size: 100px;">${row.TALLA}</h1>
+                                <h1 class="text-dark font-weight-bold pt-3 mb-0" style="font-size: 100px;">${cantidad}</h1>
                             </div>
                         </div>
                     </div>  
                 </div>
                 `);
 
-                cantidad = cantidad - capacidad;
+                if(pageBreak == 1){
+                    $('.contenedorEtiquetas').append(`
+                        <div class="page-break"></div> 
+                    `);
+                    pageBreak = 0;
+                } else {
+                    pageBreak ++;
+                }
 
-            }
+                console.log(pageBreak);
+                
+            });
 
-            $('.contenedorEtiquetas').append(`
-            <div class="col-6 mb-4">
-                <div class="card">
-                    <div class="text-center pt-4 pb-2">
-                        <p>${index} de b ${result.length}</p>
-                        <div class="btn btn-lg btn-dark mb-2">PEDIDO ${row.PEDIDO_ID}</div>
-                        <h1 class="font-weight-bold">${row.NOMBRE.toUpperCase()}</h1>
-                        <h3>${row.MARCA.toProperCase()} ${row.COLOR.toProperCase()}</h3>
-                        <div class="mt-3">
-                            <h1 class="text-dark font-weight-bold py-3 mb-0 border-bottom" style="font-size: 85px;">${row.TALLA}</h1>
-                            <h1 class="text-dark font-weight-bold pt-3 mb-0" style="font-size: 85px;">${cantidad}</h1>
-                        </div>
-                    </div>
-                </div>  
-            </div>
-            `);
-
-            index++;
-
-        });
-            
         }
 
     }
+    
 });
 
 
