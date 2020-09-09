558dbec24e92ecfe97b0358bcc0f12ef7a274de2
Your NameBrayam <bvalerop@gmail.com>
2020-09-09 16:35:07 -0500

    act. despachos-parciales.php

--- back.php
+++ back.php
@@ -1,98 +1,239 @@
-  <!-- Mostramos la tabla con la información correspondiente -->
-  <div class="table-responsive-lg">
+<?php
+
+// Incluimos el header.php y components.php
+$title = 'Despachos Parciales';
+include 'components/header.php';
+include 'components/components.php';
+require_once 'backend/api/utils.php';
+
+// Agregamos los roles que se quiere que usen esta página.
+// 'ADMINISTRADOR', 'VENTAS', 'MOLINERO', 'OPERARIO', 'PRODUCCION', 'DESPACHO', 'CONTROL', 'NORSAPLAST', 'CLIENTE'
+$roles_permitidos = array('ADMINISTRADOR', 'VENTAS', 'DESPACHO');
+
+if(!in_array($_SESSION['USUARIO']['CARGO'], $roles_permitidos)){
+    include 'components/error.php';
+    include_once 'components/footer.php';
+    exit();
+}
+
+?>
+
+<!-- Incluimos el sidebar.php -->
+<?php include 'components/sidebar.php' ?>
+
+<!-- Incluimos el contenido --> 
+<div id="contenido">
+
+    <!-- Incluimos el Navbar -->
+    <?php get_navbar('Ventas', 'Despachos Parciales'); ?>
+
+    <!-- Mostramos la tabla con la información correspondiente -->
+	<div class="table-responsive text-center" style="width:100%">
+		<div id="spinner" class="spinner-border text-center" role="status">
+			<span class="sr-only">Cargando...</span>
+		</div>
+		<table class="table table-bordered text-center" id="tabla">
+			<thead class="thead-dark"></thead>
+		</table>
+	</div>
+	<!-- Fin de Tabla -->
+
+	<!-- Toast => Alertas (data-delay="700" data-autohide="false") --> 
+	<div class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-delay="2000">
+		<div class="toast-header">
+			<i class="toast-icon"></i>
+			<strong class="mr-auto toast-title"></strong>
+			<button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+			<span aria-hidden="true">&times;</span>
+			</button>
+		</div>
+		<div class="toast-body"></div>
+	</div>
+
+
+    <!-- Tabla -->
+    <div class="table-responsive-lg pb-3">
         <table class="table table-bordered text-center" id="tabla">
             <thead class="thead-dark">
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Cliente</th>
-                    <th scope="col">Tipo de Cliente</th>
-                    <th scope="col">Forma de Pago</th>
-                    <th scope="col">Fecha de Entrega</th>
-                    <th scope="col">Estado</th>
-                    <th scope="col">Opciones</th>
-                </tr>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Cliente</th>
+                <th scope="col">Tipo de Cliente</th>
+                <th scope="col">Forma de Pago</th>
+                <th scope="col">Fecha de Entrega</th>
+                <th scope="col">Opciones</th>
+            </tr>
             </thead>
             <tbody>
+            <?php
 
-                <?php
-                require_once "backend/api/db.php";
-                require_once "backend/api/utils.php";
+            require_once "backend/api/db.php";
 
-                $sql = "SELECT P.*, C.ID AS CLIENTE_ID, C.TIPO AS CLIENTE_TIPO, C.NOMBRE AS CLIENTE_NOMBRE FROM PEDIDOS P JOIN CLIENTES C ON P.CLIENTE_ID = C.ID WHERE P.ESTADO IN ('EN ANALISIS', 'PENDIENTE');";
-                $result = db_query($sql);
+            // Agarrar toda la producción que se encuentre "PENDIENTE".
+            $sql = "SELECT P.*, C.ID AS CLIENTE_ID, C.NOMBRE AS CLIENTE_NOMBRE, C.TIPO AS CLIENTE_TIPO FROM PEDIDOS P JOIN CLIENTES C ON P.CLIENTE_ID = C.ID WHERE P.ESTADO IN ('PENDIENTE');";
+            $result = db_query($sql);
 
-                foreach ($result as $row) {
+            foreach ($result as $row) {
+                echo "<tr>";
 
-                    echo "<tr id='{$row['ID']}'>
-                            <th scope='col'>{$row['ID']}</th>
-                            <td>{$row['CLIENTE_NOMBRE']}</td>
-                            <td>{$row['CLIENTE_TIPO']}</td>
-                            <td>{$row['FORMA_PAGO']}</td>
-                            <td>" . date('d-m-Y', strtotime($row['FECHA_ESTIMADA'])) . "</td>";
+                echo "<th scope='col'>{$row['ID']}</th>";
+                echo "<td>" . mb_convert_case($row['CLIENTE_NOMBRE'], MB_CASE_TITLE, "UTF-8") . "</td>";
+                echo "<td>" . mb_convert_case($row['CLIENTE_TIPO'], MB_CASE_TITLE, "UTF-8") . "</td>";
+                echo "<td>" . mb_convert_case($row['FORMA_PAGO'], MB_CASE_TITLE, "UTF-8") . "</td>";
+                echo "<td>" . date('d-m-Y', strtotime($row['FECHA_ESTIMADA'])) . "</td>";
+                echo "<td><a href='#' data-toggle='modal' data-target='#verOrdenesDespachadas' data-id='{$row['ID']}'><i class='fas fa-eye icon-color'></i></a></td>";
 
-                    // Estado => Pedidos Pendientes
-                    if ($_SESSION['USUARIO']['CARGO'] == 'ADMINISTRADOR') {
-                       
-                        if ($row['ESTADO'] === 'EN ANALISIS') {
-                            echo "<td> 
-                                    <a href='aprobar-pedido.php?id={$row['ID']}' class='btn btn-sm btn-main'>Aprobar Pedido</a>
-                                </td>";
-                        } else {
-                            echo "<td>Pendiente</td>";
-                        }
-
-                    } else {
-                        $row['ESTADO'] == 'EN ANALISIS' ? print "<td>En analisis</td>" : print "<td>Pendiente</td>";
-                    }
-
-                    // Opciones => Pedidos Pendientes.
-                    echo "<td>";
-
-                    if ($_SESSION['USUARIO']['CARGO'] == 'ADMINISTRADOR') {
-
-                        if ($row['ESTADO'] === 'EN ANALISIS') {
-
-                            echo "<a href='editar-pedido.php?id={$row['ID']}' class='mr-1'>
-                                    <i class='fas fa-edit icon-color'></i>
-                                </a>
-                                <a href='javascript:void(0)' class='eliminarPedido mr-1' data-id='{$row['ID']}'>
-                                    <i class='fas fa-trash icon-color'></i>
-                                </a>";
-
-                        } else {
-
-                            echo "<a href='javascript:void(0)' class='cancelarPedido mr-1' data-id='{$row['ID']}'>
-                                    <i class='fas fa-ban icon-color'></i>
-                                </a>";
-
-                        }
-
-                        echo "<a href='javascript:void(0)' data-toggle='modal' data-target='#verPedido' data-id='{$row['ID']}'>
-                                <i class='fas fa-eye icon-color'></i>
-                            </a>";
-
-                    }
-
-                    if ($_SESSION['USUARIO']['CARGO'] == 'VENTAS' || $_SESSION['USUARIO']['CARGO'] == 'DESPACHO') {
-
-                        if ($row['ESTADO'] === 'EN ANALISIS') {
-                            echo "<a href='editar-pedido.php?id={$row['ID']}'>
-                                    <i class='fas fa-edit icon-color'></i>
-                                </a>";
-                        }
-
-                        echo "<a href='javascript:void(0)' data-toggle='modal' data-target='#verPedido' data-id='{$row['ID']}'>
-                                <i class='fas fa-eye icon-color'></i>
-                            </a>";
-
-                    }
-
-                    echo "</td></tr>";
-
-                }
-
-                ?>
+                echo "</tr>";
+            }
+            ?>
             </tbody>
         </table>
     </div>
-    <!-- / Fin de tabla -->
+    <!-- End of Table -->
+
+    <!-- Modal de Ver Orderes Despachadas -->
+    <div class="modal fade" id="verOrdenesDespachadas" tabindex="-1" role="dialog"
+            aria-labelledby="verOrdenesDespachadas" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+            <div class="modal-content">
+
+                <!-- Form -->
+                <form action="backend/api/pedidos/despachar.php" method="POST" id="verificarCheckBoxes">
+
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel"><i
+                                    class="fas fa-shopping-bag icon-color"></i> Datos del Pedido</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+
+                    <div class="modal-body">
+                        <!-- Table -->
+                        <div class="table-responsive-lg py-3">
+                            <table class="table table-bordered text-center" id="tabla-modal">
+                                <thead class="thead-dark">
+                                <tr>
+                                    <th scope="col">Marca</th>
+                                    <th scope="col">Talla</th>
+                                    <th scope="col">Color</th>
+                                    <th scope="col">Total</th>
+                                    <th scope="col">Disponible para despachar</th>
+                                    <th scope="col">Despachado</th>
+                                    <th scope="col">Opciones</th>
+                                </tr>
+                                </thead>
+                                <tbody id="tabla-tbody-modal">
+                                </tbody>
+                            </table>
+                        </div>
+                        <!-- End of Table -->
+                    </div>
+
+                    <div class="modal-footer">
+                        <?php
+                            if ($_SESSION['USUARIO']['CARGO'] == 'ADMINISTRADOR' || $_SESSION['USUARIO']['CARGO'] == 'DESPACHO'): 
+                        ?>
+                        <button type="button" class="btn btn-sm btn-secondary" data-dismiss="modal">Cerrar</button>
+                        <button type="submit" id="submit_modal" class="btn btn-sm btn-main">Entregar Pedido Parcial </button>
+                        <?php 
+                            endif;
+                        ?>
+                    </div>
+
+                </form>
+                <!-- End of Form -->
+
+            </div>
+        </div>
+    </div>
+    <!-- / Fin de Modal -->
+
+</div>
+<!-- / Fin de Contenido-->
+
+<!-- Inline JavaScript -->
+<script>
+
+// DataTables Plugin: https://datatables.net/
+const tabla = $('#tabla').DataTable({
+    info: false,
+    dom: "lrtip",
+    // searching: false,
+    lengthChange: false,
+    pageLength: 5,
+    order: [[0, 'desc']],
+    columnDefs: [{
+        targets: 4,
+        searchable: true,
+        orderable: true,
+        className: "align-middle", "targets": "_all"
+    }],
+    language: {
+        "url": "<?= BASE_URL . "datatables/Spanish.json"; ?>"
+    }
+});
+
+$('#verOrdenesDespachadas').on('show.bs.modal', function (e) {
+
+    let pedidoId = $(e.relatedTarget).data('id');
+
+    $.ajax({
+        type: 'post',
+        url: 'backend/api/utils.php?fun=obtenerProduccionReferencia',
+        data: 'pedido_id=' + pedidoId,
+        success: function (data) {
+
+            const result = JSON.parse(data);
+            const tabla = $('#tabla-modal > tbody:last-child');
+            tabla.empty();
+
+            result.forEach(row => {
+
+                if (row.ESTADO === 'COMPLETADO') {
+                    row.ESTADO = `
+                    <div class="form-check p-0">
+                        <i class="fas fa-check"></i>
+                    </div>
+                    `;
+                } else {
+                    row.ESTADO = `
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" name="producción-id-${row.ID}" value="${row.ID}">
+                    </div>
+                    `;
+                }
+
+                tabla.append(`<tr>
+                    <td>${row.SUELA_MARCA.toProperCase()}</td>
+                    <td>${row.SUELA_TALLA}</td>
+                    <td>${row.SUELA_COLOR.toProperCase()}</td>
+                    <td>${row.CANTIDAD}</td>
+                    <td>${row.DISPONIBLE}</td>
+                    <td>${row.DESPACHADO}</td>
+                    <td>
+                        ${row.ESTADO}
+                    </td>
+                </tr>`);
+            });
+        }
+    });
+
+}).on('submit', function (e) {
+
+    console.log($('#verificarCheckBoxes input[type=checkbox]:checked').length);
+
+    if ($('#verificarCheckBoxes input[type=checkbox]:checked').length === 0) {
+
+        e.preventDefault();
+
+        return Swal.fire("Whoops", "Debes marcar un pedido primero.", "warning");
+
+    }
+
+});
+
+</script>
+
+<!-- Incluimos el footer.php -->
+<?php include_once 'components/footer.php'; ?>
--- despachos-parciales.php
+++ despachos-parciales.php
@@ -27,99 +27,42 @@ if(!in_array($_SESSION['USUARIO']['CARGO'], $roles_permitidos)){
     <!-- Incluimos el Navbar -->
     <?php get_navbar('Ventas', 'Despachos Parciales'); ?>
 
-    <!-- Tabla -->
-    <div class="table-responsive-lg pb-3">
-        <table class="table table-bordered text-center" id="tabla">
-            <thead class="thead-dark">
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Cliente</th>
-                <th scope="col">Tipo de Cliente</th>
-                <th scope="col">Forma de Pago</th>
-                <th scope="col">Fecha de Entrega</th>
-                <th scope="col">Opciones</th>
-            </tr>
-            </thead>
-            <tbody>
-            <?php
+    <!-- Mostramos la tabla con la información correspondiente -->
+	<div class="table-responsive text-center" style="width:100%">
+		<div id="spinner" class="spinner-border text-center" role="status">
+			<span class="sr-only">Cargando...</span>
+		</div>
+		<table class="table table-bordered text-center" id="tabla">
+			<thead class="thead-dark"></thead>
+		</table>
+	</div>
+	<!-- Fin de Tabla -->
 
-            require_once "backend/api/db.php";
-
-            // Agarrar toda la producción que se encuentre "PENDIENTE".
-            $sql = "SELECT P.*, C.ID AS CLIENTE_ID, C.NOMBRE AS CLIENTE_NOMBRE, C.TIPO AS CLIENTE_TIPO FROM PEDIDOS P JOIN CLIENTES C ON P.CLIENTE_ID = C.ID WHERE P.ESTADO IN ('PENDIENTE');";
-            $result = db_query($sql);
-
-            foreach ($result as $row) {
-                echo "<tr>";
-
-                echo "<th scope='col'>{$row['ID']}</th>";
-                echo "<td>" . mb_convert_case($row['CLIENTE_NOMBRE'], MB_CASE_TITLE, "UTF-8") . "</td>";
-                echo "<td>" . mb_convert_case($row['CLIENTE_TIPO'], MB_CASE_TITLE, "UTF-8") . "</td>";
-                echo "<td>" . mb_convert_case($row['FORMA_PAGO'], MB_CASE_TITLE, "UTF-8") . "</td>";
-                echo "<td>" . date('d-m-Y', strtotime($row['FECHA_ESTIMADA'])) . "</td>";
-                echo "<td><a href='#' data-toggle='modal' data-target='#verOrdenesDespachadas' data-id='{$row['ID']}'><i class='fas fa-eye icon-color'></i></a></td>";
-
-                echo "</tr>";
-            }
-            ?>
-            </tbody>
-        </table>
-    </div>
-    <!-- End of Table -->
+	<!-- Toast => Alertas (data-delay="700" data-autohide="false") --> 
+	<div class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-delay="2000">
+		<div class="toast-header">
+			<i class="toast-icon"></i>
+			<strong class="mr-auto toast-title"></strong>
+			<button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+			<span aria-hidden="true">&times;</span>
+			</button>
+		</div>
+		<div class="toast-body"></div>
+	</div>
 
     <!-- Modal de Ver Orderes Despachadas -->
-    <div class="modal fade" id="verOrdenesDespachadas" tabindex="-1" role="dialog"
-            aria-labelledby="verOrdenesDespachadas" aria-hidden="true">
+    <div id ="verDespachosModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="verDespachosModal" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
             <div class="modal-content">
-
-                <!-- Form -->
                 <form action="backend/api/pedidos/despachar.php" method="POST" id="verificarCheckBoxes">
-
                     <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel"><i
-                                    class="fas fa-shopping-bag icon-color"></i> Datos del Pedido</h5>
+                        <h5 class="modal-title"><i class="fas fa-shopping-bag icon-color"></i> Pedidos Despachados</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
                     </div>
-
-                    <div class="modal-body">
-                        <!-- Table -->
-                        <div class="table-responsive-lg py-3">
-                            <table class="table table-bordered text-center" id="tabla-modal">
-                                <thead class="thead-dark">
-                                <tr>
-                                    <th scope="col">Marca</th>
-                                    <th scope="col">Talla</th>
-                                    <th scope="col">Color</th>
-                                    <th scope="col">Total</th>
-                                    <th scope="col">Disponible para despachar</th>
-                                    <th scope="col">Despachado</th>
-                                    <th scope="col">Opciones</th>
-                                </tr>
-                                </thead>
-                                <tbody id="tabla-tbody-modal">
-                                </tbody>
-                            </table>
-                        </div>
-                        <!-- End of Table -->
-                    </div>
-
-                    <div class="modal-footer">
-                        <?php
-                            if ($_SESSION['USUARIO']['CARGO'] == 'ADMINISTRADOR' || $_SESSION['USUARIO']['CARGO'] == 'DESPACHO'): 
-                        ?>
-                        <button type="button" class="btn btn-sm btn-secondary" data-dismiss="modal">Cerrar</button>
-                        <button type="submit" id="submit_modal" class="btn btn-sm btn-main">Entregar Pedido Parcial </button>
-                        <?php 
-                            endif;
-                        ?>
-                    </div>
-
+                    <div id="detallesDespachos" class="modal-body"></div>
                 </form>
-                <!-- End of Form -->
-
             </div>
         </div>
     </div>
@@ -131,83 +74,149 @@ if(!in_array($_SESSION['USUARIO']['CARGO'], $roles_permitidos)){
 <!-- Inline JavaScript -->
 <script>
 
-// DataTables Plugin: https://datatables.net/
-const tabla = $('#tabla').DataTable({
-    info: false,
-    dom: "lrtip",
-    // searching: false,
-    lengthChange: false,
-    pageLength: 5,
-    order: [[0, 'desc']],
-    columnDefs: [{
-        targets: 4,
-        searchable: true,
-        orderable: true,
-        className: "align-middle", "targets": "_all"
-    }],
-    language: {
-        "url": "<?= BASE_URL . "datatables/Spanish.json"; ?>"
+// VARIABLES => Declarando Variables Globales.
+var tabla;
+var posicionTabla;
+
+// DATATABLES => Mostrando la tabla DESPACHOS_PARCIALES.
+$.ajax({
+    type: 'get',
+    url: 'backend/api/utils.php?fun=obtenerPedidosPendientes',
+    async: false,
+    success: function (data) {
+
+        const result = JSON.parse(data);
+
+        tabla = $('#tabla').DataTable({
+            "initComplete": function(settings, json) {
+                $("#spinner").css('visibility', 'hidden');
+            },
+            "info": false,
+            "dom": "lrtip",
+            "pageLength": 6,
+            "lengthChange": false,
+            "order": [[0, 'desc']],
+            "data": result,
+            "columns": [
+                { data: "ID", title: "#" },
+				{ data: "CLIENTE_NOMBRE", title: "Cliente" },
+                { data: "CLIENTE_TIPO", title: "Tipo" },
+                { data: "FORMA_PAGO", title: "Forma Pago" },
+				{ data: "FECHA_ESTIMADA", title: "Fecha Estimada" },
+                { data: 'ID', title: "Opciones",
+					render: function(value, type, row) {
+                        
+                        return `<a href='javascript:void(0)' class='verDespachos' data-id='${row.ID}'>
+                                <i class='fas fa-eye icon-color'></i>
+                            </a>`;
+  
+                	}
+                },
+            ],
+            "columnDefs": [{
+                searchable: true,
+                orderable: true,
+                className: "align-middle", "targets": "_all"
+            }],
+            "language": {
+                "url": "<?= BASE_URL . "datatables/Spanish.json"; ?>"
+            }
+        });
+
+        // DATATABLES => Paginación
+        $.fn.DataTable.ext.pager.numbers_length = 5;
+	   
     }
+
 });
 
-$('#verOrdenesDespachadas').on('show.bs.modal', function (e) {
+// DATATABLES => Detectar Fila Actual (Aplica para Eliminar y Editar un Elemento)
+$('#tabla tbody').on( 'click', 'tr', function () { 
+	posicionTabla = this;
+});
 
-    let pedidoId = $(e.relatedTarget).data('id');
+
+
+// VER => Ver un Pedido.
+$('#tabla tbody').on( 'click', '.verDespachos', function () { 
+    
+    let id = $(this).data("id");
 
     $.ajax({
         type: 'post',
-        url: 'backend/api/utils.php?fun=obtenerProduccionReferencia',
-        data: 'pedido_id=' + pedidoId,
+        url: 'backend/api/despachos/ver.php',
+        data: 'pedido_id=' + id,
+        async: false,
         success: function (data) {
 
-            const result = JSON.parse(data);
-            const tabla = $('#tabla-modal > tbody:last-child');
-            tabla.empty();
+            document.getElementById('detallesDespachos').innerHTML = data;
+            $('#verDespachosModal').modal('show');
 
-            result.forEach(row => {
-
-                if (row.ESTADO === 'COMPLETADO') {
-                    row.ESTADO = `
-                    <div class="form-check p-0">
-                        <i class="fas fa-check"></i>
-                    </div>
-                    `;
-                } else {
-                    row.ESTADO = `
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="producción-id-${row.ID}" value="${row.ID}">
-                    </div>
-                    `;
-                }
-
-                tabla.append(`<tr>
-                    <td>${row.SUELA_MARCA.toProperCase()}</td>
-                    <td>${row.SUELA_TALLA}</td>
-                    <td>${row.SUELA_COLOR.toProperCase()}</td>
-                    <td>${row.CANTIDAD}</td>
-                    <td>${row.DISPONIBLE}</td>
-                    <td>${row.DESPACHADO}</td>
-                    <td>
-                        ${row.ESTADO}
-                    </td>
-                </tr>`);
-            });
         }
+
     });
 
-}).on('submit', function (e) {
-
-    console.log($('#verificarCheckBoxes input[type=checkbox]:checked').length);
-
-    if ($('#verificarCheckBoxes input[type=checkbox]:checked').length === 0) {
-
-        e.preventDefault();
-
-        return Swal.fire("Whoops", "Debes marcar un pedido primero.", "warning");
-
-    }
-
 });
+
+
+// $('#verOrdenesDespachadas').on('show.bs.modal', function (e) {
+
+//     let pedidoId = $(e.relatedTarget).data('id');
+
+//     $.ajax({
+//         type: 'post',
+//         url: 'backend/api/utils.php?fun=obtenerProduccionReferencia',
+//         data: 'pedido_id=' + pedidoId,
+//         success: function (data) {
+
+//             const result = JSON.parse(data);
+//             const tabla = $('#tabla-modal > tbody:last-child');
+//             tabla.empty();
+
+//             result.forEach(row => {
+
+//                 if (row.ESTADO === 'COMPLETADO') {
+//                     row.ESTADO = `
+//                     <div class="form-check p-0">
+//                         <i class="fas fa-check"></i>
+//                     </div>
+//                     `;
+//                 } else {
+//                     row.ESTADO = `
+//                     <div class="form-check">
+//                         <input class="form-check-input" type="checkbox" name="producción-id-${row.ID}" value="${row.ID}">
+//                     </div>
+//                     `;
+//                 }
+
+//                 tabla.append(`<tr>
+//                     <td>${row.SUELA_MARCA.toProperCase()}</td>
+//                     <td>${row.SUELA_TALLA}</td>
+//                     <td>${row.SUELA_COLOR.toProperCase()}</td>
+//                     <td>${row.CANTIDAD}</td>
+//                     <td>${row.DISPONIBLE}</td>
+//                     <td>${row.DESPACHADO}</td>
+//                     <td>
+//                         ${row.ESTADO}
+//                     </td>
+//                 </tr>`);
+//             });
+//         }
+//     });
+
+// }).on('submit', function (e) {
+
+//     console.log($('#verificarCheckBoxes input[type=checkbox]:checked').length);
+
+//     if ($('#verificarCheckBoxes input[type=checkbox]:checked').length === 0) {
+
+//         e.preventDefault();
+
+//         return Swal.fire("Whoops", "Debes marcar un pedido primero.", "warning");
+
+//     }
+
+// });
 
 </script>
 
