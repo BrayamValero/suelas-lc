c3380875938c55141bbe2cdac0958fcc243adf56
Your NameBrayam <bvalerop@gmail.com>
2020-09-30 08:08:25 -0500

    Update series.php

--- series.php
+++ series.php
@@ -27,43 +27,27 @@ if(!in_array($_SESSION['USUARIO']['CARGO'], $roles_permitidos)){
     <?php get_navbar('Inventario', 'Series'); ?>
 
     <!-- Mostramos la tabla con la información correspondiente -->
-	<div class="table-responsive-lg">
+	<div class="table-responsive text-center" style="width:100%">
+		<div id="spinner" class="spinner-border text-center" role="status">
+			<span class="sr-only">Cargando...</span>
+		</div>
 		<table class="table table-bordered text-center" id="tabla">
-			<thead class="thead-dark">
-				<tr>
-					<th scope="col">#</th>
-					<th scope="col">Nombre</th>
-					<th scope="col">Opciones</th>
-				</tr>
-			</thead>
-			<tbody>
-				<?php
-					require_once "backend/api/db.php";
-					$sql = "SELECT * FROM SERIES;";
-					$result = db_query($sql);
-
-					foreach ($result as $row) {
-						echo "<tr>";
-
-						echo "<th scope=\"col\">{$row['ID']}</th>";
-						echo "<td>". mb_convert_case($row['NOMBRE'], MB_CASE_TITLE, "UTF-8") ."</td>";
-						
-						echo "<td>
-								<a href='#' data-toggle='modal' data-target='#verSerieModal' data-id='{$row['ID']}'>
-									<i class='fas fa-eye icon-color'></i>
-								</a>
-								<a href='#' data-toggle='modal' data-target='#editarSerieModal' data-id='{$row['ID']}'>
-									<i class='fas fa-edit icon-color'></i>
-								</a>
-							</td>";
-
-						echo "</tr>";
-					}
-				?>
-			</tbody>
+			<thead class="thead-dark"></thead>
 		</table>
 	</div>
 	<!-- Fin de Tabla -->
+
+	<!-- Toast => Alertas (data-delay="700" data-autohide="false") --> 
+	<div class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-delay="2000">
+		<div class="toast-header">
+			<i class="toast-icon"></i>
+			<strong class="mr-auto toast-title"></strong>
+			<button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+			<span aria-hidden="true">&times;</span>
+			</button>
+		</div>
+		<div class="toast-body"></div>
+	</div>
 
 	<!-- Modal de añadir Serie -->
 	<div class="modal fade" id="añadirSerieModal" tabindex="-1" role="dialog" aria-labelledby="añadirSerieModal" aria-hidden="true">
@@ -205,26 +189,66 @@ if(!in_array($_SESSION['USUARIO']['CARGO'], $roles_permitidos)){
 <!-- Inline JavaScript -->
 <script>
 
-// DataTables Plugin: https://datatables.net/
-const tabla = $('#tabla').DataTable({
-	info: false,
-	dom: "lrtip",
-	// searching: false,
-	lengthChange: false,
-	pageLength: 10,
-	order: [[0, 'desc']],
-	columnDefs: [{
-		targets: 4,
-		searchable: true,
-		orderable: true,
-		className: "align-middle", "targets": "_all"
-	}],
-	language: {
-		"url": "<?= BASE_URL . "datatables/Spanish.json"; ?>"
-	}
+var tabla;
+var posicionTabla;
+
+// DATATABLES => Mostrando la tabla STOCK.
+$.ajax({
+    type: 'get',
+    url: 'backend/api/utils.php?fun=obtenerSeries',
+    async: false,
+    success: function (data) {
+
+        const result = JSON.parse(data);
+
+        tabla = $('#tabla').DataTable({
+            "initComplete": function(settings, json) {
+                $("#spinner").css('display', 'none');
+            },
+            "info": false,
+            "dom": "lrtip",
+            "pageLength": 10,
+            "lengthChange": false,
+            "order": [[0, 'desc']],
+            "data": result,
+            "columns": [
+                { data: "ID", title: "#" },
+                { data: "NOMBRE", title: "Nombre" },
+                { 
+                    data: 'ID',
+                    title: "Opciones", render: function(value, type, row) {
+                        return `<a href='javascript:void(0)' data-id='${row.ID}' data-toggle='modal' data-target='#editarSerieModal'>
+									<i class='fas fa-edit icon-color mr-1'></i>
+								</a>
+								<a href='javascript:void(0)' data-id='${row.ID}' data-toggle='modal' data-target='#verSerieModal'>
+									<i class='fas fa-eye icon-color mr-1'></i>
+								</a>`;
+                    }
+                },
+            ],
+            "columnDefs": [{
+                searchable: true,
+                orderable: true,
+                className: "align-middle", "targets": "_all"
+            }],
+            "language": {
+                "url": "<?= BASE_URL . "datatables/Spanish.json"; ?>"
+            }
+        });
+
+        // DATATABLES => Paginación
+        $.fn.DataTable.ext.pager.numbers_length = 5;
+        
+    }
+
 });
 
-// Select2 => Dependencia de este archivo 
+// DATATABLES => Detectar Fila Actual (Aplica para Eliminar y Editar un Elemento)
+$('#tabla tbody').on( 'click', 'tr', function () { 
+	posicionTabla = this;
+});
+
+// Select2 => Dependencia de este archivo
 $(document).ready(function () {
 	$('.select-multiple').select2({
 		// dropdownParent: $('#example'),
@@ -238,51 +262,54 @@ $(document).ready(function () {
 	});
 });
 
-// 1. Modal de Añadir Serie
+// VISTA => Modal de añadir serie.
 $('#añadirSerieModal').on('show.bs.modal', function (e) {
 	// Borramos las opciones del select2
 	$('.select-multiple').val(null).trigger('change');
 });
 
-// 2. Modal de Editar Serie
+
+// EDITAR => Editar una serie.
 $('#editarSerieModal').on('show.bs.modal', function (e) {
 
-	let serie_id = $(e.relatedTarget).data('id');
+	let id = $(e.relatedTarget).data('id');
 
 	$('.select-multiple').val(null).trigger('change');
 
 	$.ajax({
-		url: `backend/api/utils.php?fun=obtenerSerie&id=${serie_id}`,
+		url: `backend/api/utils.php?fun=obtenerSerie&id=${id}`,
 		success: function (data) {
 
 			const series = [];
 			const result = JSON.parse(data);
-			$('#editarSerieId').val(serie_id);
-			$('#editarNombre').val(result[0].NOMBRE.toProperCase());
+
+			document.getElementById('editarSerieId').value = id;
+			document.getElementById('editarNombre').value = result[0].NOMBRE.toProperCase();
 
 			result.forEach(row => {
 				series.push(row.SUELA_ID);
 			});	
-			
+
 			$('.select-multiple').val(series);
 			$('.select-multiple').trigger('change');
-
+	
 		}
 	});
 
 });
 
-// 3. Modal de Ver una serie.
+// VER => Ver una serie.
 $('#verSerieModal').on('show.bs.modal', function (e) {
 	
-	let serie_id = $(e.relatedTarget).data('id');
+	let id = $(e.relatedTarget).data('id');
 
 	$.ajax({
-		url: `backend/api/utils.php?fun=obtenerSerie&id=${serie_id}`,
+		url: `backend/api/utils.php?fun=obtenerSerie&id=${id}`,
 		success: function (data) {
 
-			const result = JSON.parse(data);
+			const result = JSON.parse(data);	
 			document.getElementById('verNombreSerieModal').textContent = result[0].NOMBRE.toProperCase();
+
 			const tabla = $('#verTablaSerieModal > tbody:last-child');
 			tabla.empty();
 
