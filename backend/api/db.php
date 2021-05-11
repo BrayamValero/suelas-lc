0d796fb51f0f64ba5ee9e470dfff6ba2c7382bc8
Brayam Valero <bvalerop@gmail.com>
2021-05-11 15:02:24 -0500

    Update db.php

--- backend/api/db.php
+++ backend/api/db.php
@@ -11,7 +11,7 @@ if(strtoupper(substr(PHP_OS, 0 ,3)) == 'WIN'){
     define('DB', array(
         'dsn' => 'mysql:host=localhost;dbname=SUELAS;charset=utf8',
         'user' => 'root',
-        'pass' => 'root'
+        'pass' => '' // Empty Pass (Working with XAMPP)
     ));
 } else{
     define('DB', array(
